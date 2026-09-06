437dae07e723ad652354cf466aabd1b5510a3d9d
Martin Beahan <martinbeahan@users.noreply.github.com>
2026-09-06 23:55:39 +0000

    Remove invalid Material inset style attrs so resources link.
    insetLeft/Right/Top/Bottom are not app attrs in this theme setup and broke mergeDebugResources.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.fintrack.dndbeginnerremote"
         minSdk = 30
         targetSdk = 35
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
